c1d992fcd1eaa17e95a7092e22da0a9cfa132589
Sebastian Zilles <dev@zilles.io>
2024-01-31 12:25:57 +0100

    Beispiel Anfrage in der ExamplePage hinzufügen

--- src/js/pages/example/ExamplePageState.tsx
+++ src/js/pages/example/ExamplePageState.tsx
@@ -1,8 +1,16 @@
 import { RouteContext } from "@opendash/router";
 import { makeAutoObservable } from "mobx";
 
+interface Product {
+  id: string;
+  name: string;
+  self_link: string;
+}
+
 export class ExamplePageState {
   ctx: RouteContext;
+
+  products: Product[] = [];
 
   constructor(ctx: RouteContext) {
     this.ctx = ctx;
@@ -11,5 +19,19 @@ export class ExamplePageState {
     ctx.setDescription(undefined);
 
     makeAutoObservable(this);
+
+    this.init();
+  }
+
+  async init() {
+    const response = await fetch("https://api.predic8.de/shop/v2/products");
+
+    const data = await response.json();
+
+    this.setProducts(data.products);
+  }
+
+  setProducts(products: Product[]) {
+    this.products = products;
   }
 }
