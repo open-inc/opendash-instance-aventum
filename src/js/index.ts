e16acd1754650bde2d9e84a72cee8d2824e5a97b
Sebastian Zilles <dev@zilles.io>
2024-01-31 11:49:37 +0100

    Grundgerüst für eine open.DASH App hinzufügen Hier wird in der src/js/index.ts die "Factory" für open.DASH ausgeführt. In der Factory kann die Anwendung konfiguriert werden, bevor diese gestartet wird. In der Factory werden zum Beispiel Sprachdateien, Plugins und Adapter registriert. Plugins können Logik für die Factory kapseln. Für die App könnte entweder ein Plugin entwickelt werden oder die komplette Anwendungslogik in der Factory registiert werden.

---
+++ src/js/index.ts
@@ -0,0 +1,50 @@
+import "antd/dist/reset.css";
+
+import "./parse.config";
+
+import { init, StorageAdapterLS } from "@opendash/core";
+import { registerIconPack } from "@opendash/icons";
+import { ParsePlugin } from "@opendash/plugin-parse";
+
+init("opendash", async (factory) => {
+  // Icons
+  // @ts-ignore
+  registerIconPack(await import("@opendash/icons/dist/fa-regular.json"));
+
+  // Languages
+  factory.registerLanguage("en", "English");
+  factory.registerLanguage("de", "Deutsch", "en", true);
+
+  // Ant Design Translations
+  factory.registerAntDesignTranslation(
+    "en",
+    () => import("antd/lib/locale/en_US")
+  );
+  factory.registerAntDesignTranslation(
+    "de",
+    () => import("antd/lib/locale/de_DE")
+  );
+
+  // App Translations
+  factory.registerTranslationResolver(
+    "en",
+    "app",
+    async () => await import("./translations/app/en.json")
+  );
+  factory.registerTranslationResolver(
+    "de",
+    "app",
+    async () => await import("./translations/app/de.json")
+  );
+
+  // Adapter + Plugins
+  factory.registerDeviceStorageAdapter(new StorageAdapterLS());
+
+  await factory.use(
+    new ParsePlugin({
+      authLdapActive: false,
+    })
+  );
+}).then((app) => {
+  console.log("init open.DASH");
+});
